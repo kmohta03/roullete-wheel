bf5b7e711a95071bf1bb5058c922f3abff1ea735
Kushal Mohta <km578@duke.edu>
2024-04-23 07:08:31 -0400

    test

--- assembly_files/led_number_generator.s
+++ assembly_files/led_number_generator.s
@@ -542,7 +542,7 @@ bet_extraction_init:
     # Start extracting bets
 extract_bets:
     # Check if all bets have been processed
-    addi $t3, $zero, 12
+    addi $t3, $zero, 12 # WORKS UNTIL HERE
     bne $t0, $t3, continue_extraction
     j end_extraction
 
@@ -561,30 +561,32 @@ extract_from_reg_27:
 
 extract_from_reg_25:
     # Extract bet from register 25
-    addi $t0, $zero, 4278190080  # Load the mask value into $t0 (4278190080 is 0xFF000000 in decimal)
-    and $t2, $25, $t0            # Extract first 8 bits
+    addi $t4, $zero, 255  # Load the mask value into $t0 (4278190080 is 0xFF000000 in decimal)
+    and $t2, $25, $t4            # Extract first 8 bits
     addi $28, $t2, 0
+    addi $t0, $t2, 0
     sra $25, $25, 8
+    addi $29, $zero, 3
+    j extract_from_reg_25
     j process_bet
 
 extract_bet_reg_26:
     # Extract bet from register 26
-    addi $t0, $zero, 4278190080  # Load the mask value into $t0 (4278190080 is 0xFF000000 in decimal)
-    and $t2, $26, $t0
+    addi $t4, $zero, 255  # Load the mask value into $t0 (4278190080 is 0xFF000000 in decimal)
+    and $t2, $26, $t4
     sra $26, $26, 8            # Extract first 8 bits
     j process_bet
 
 extract_bet_reg_27:
     # Extract bet from register 27
-    addi $t0, $zero, 4278190080  # Load the mask value into $t0 (4278190080 is 0xFF000000 in decimal)
-    and $t2, $27, $t0 
+    addi $t4, $zero, 255  # Load the mask value into $t0 (4278190080 is 0xFF000000 in decimal)
+    and $t2, $27, $t4
     sra $27, $27, 8           # Extract first 8 bits
     j process_bet
 
 process_bet:
     # Check if extracted bet is zero (end of bets)
-    addi $29, $zero, 3
-    j process_bet
+    
     bne $t2, $zero, calculate_payout
     j end_extraction
 
